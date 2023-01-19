086ac5f4cc6161e0aad48800154f2a4f9954912d
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-01-19 23:11:10 +0200

    Clear code

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,41 +2,41 @@ import React from 'react';
 import Button from './Button'
 
 class ButtonPanel extends React.Component {
-    render() {
-        return (
-            <div className='component-button-panel'>
-                <div>
-                  <Button name='AC' />
-                  <Button name='+/-' />
-                  <Button name='%' />
-                  <Button name='÷' orange />
-                </div>
-                <div>
-                  <Button name='7' />
-                  <Button name='8' />
-                  <Button name='9' />
-                  <Button name='x' orange />
-                </div>
-                <div>
-                  <Button name='4' />
-                  <Button name='5' />
-                  <Button name='6' />
-                  <Button name='-' orange />
-                </div>
-                <div>
-                  <Button name='1' />
-                  <Button name='2' />
-                  <Button name='3' />
-                  <Button name='+' orange />
-                </div>
-                <div>
-                  <Button name='0' wide />
-                  <Button name='.' />
-                  <Button name='=' orange />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='component-button-panel'>
+        <div>
+          <Button name='AC' />
+          <Button name='+/-' />
+          <Button name='%' />
+          <Button name='÷' orange />
+        </div>
+        <div>
+          <Button name='7' />
+          <Button name='8' />
+          <Button name='9' />
+          <Button name='x' orange />
+        </div>
+        <div>
+          <Button name='4' />
+          <Button name='5' />
+          <Button name='6' />
+          <Button name='-' orange />
+        </div>
+        <div>
+          <Button name='1' />
+          <Button name='2' />
+          <Button name='3' />
+          <Button name='+' orange />
+        </div>
+        <div>
+          <Button name='0' wide />
+          <Button name='.' />
+          <Button name='=' orange />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default ButtonPanel;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,18 +5,18 @@ import './Calculator.css'
 
 
 class Calculator extends React.Component {
-    render(){
-        return (
-            <div className='component-app'>
-                {/* display zone */}
-                <div style={{width : 300}}>
-                    <h3 style={{paddingLeft:15}}>000</h3>
-                </div>
-                {/* buttons and signs */}
-                <ButtonPanel />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='component-app'>
+        {/* display zone */}
+        <div style={{ width: 300 }}>
+          <h3 style={{ paddingLeft: 15 }}>000</h3>
+        </div>
+        {/* buttons and signs */}
+        <ButtonPanel />
+      </div>
+    )
+  }
 }
 
 export default Calculator;
