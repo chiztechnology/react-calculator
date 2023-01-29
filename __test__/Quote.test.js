376d815367f929251f4973e0e1256cb00ef85ac8
Israel CHIZUNGU <chizunguisrael@gmail.com>
2023-01-29 03:09:49 +0200

    Fix errors

--- __test__/Quote.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import Quote from '../src/components/Quote';
-
-it('renders correctly when there are no items', () => {
-  const tree = renderer.create(<Quote />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
